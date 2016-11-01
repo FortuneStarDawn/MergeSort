640f8fff55287b3d9623ac887d1526bfb24f6f7e
FortuneStarDawn <yoyo6734@yahoo.com.tw>
2016-11-01 23:32:37 +0800

    add non-recursive

---
+++ Non-recursive/mergeSort.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector<int> mergeSort(vector<int> A);
+
+int main()
+{
+    string line;
+    stringstream ss;
+    int element;
+    vector<int> A;
+
+    getline(cin, line);
+    ss<<line;
+    while(ss>>element) A.push_back(element);
+
+    A = mergeSort(A);
+    for(int i=0; i<A.size(); i++) cout<<A.at(i)<<' ';
+    cout<<endl;
+    return 0;
+}
+
+vector<int> mergeSort(vector<int> A)
+{
+    int left, right, mid, end, now;
+    vector<int> temp = A;
+
+    for(int width=1; width<A.size(); width*=2)
+    {
+        for(int start=0; start<A.size(); start+=2*width)
+        {
+            mid = start + width -1;
+            end = start + 2*width -1;
+
+            if(mid >= A.size()) mid = A.size()-1;
+            if(end >= A.size()) end = A.size()-1;
+
+            left = start;
+            right = mid + 1;
+            now = start;
+
+            while(left<=mid && right<=end)
+            {
+                if(A.at(left) <= A.at(right))
+                {
+                    temp.at(now) = A.at(left);
+                    left++;
+                }
+                else
+                {
+                    temp.at(now) = A.at(right);
+                    right++;
+                }
+                now++;
+            }
+            while(left<=mid)
+            {
+                temp.at(now) = A.at(left);
+                left++;
+                now++;
+            }
+            while(right<=end)
+            {
+                temp.at(now) = A.at(right);
+                right++;
+                now++;
+            }
+        }
+        A = temp;
+    }
+    return A;
+}
--- Recursive/mergeSort.cpp
+++ Recursive/mergeSort.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 vector<int> mergeSort(vector<int> A);
+vector<int> merge(vector<int> left, vector<int> right);
 
 int main()
 {
@@ -32,29 +33,19 @@ vector<int> mergeSort(vector<int> A)
     right.assign(A.begin() + A.size()/2, A.end());
     leftSorted  = mergeSort(left);
     rightSorted = mergeSort(right);
+    all = merge(leftSorted, rightSorted);
+    return all;
+}
+
+vector<int> merge(vector<int> left, vector<int> right)
+{
+    vector<int> all;
     vector<int>::iterator itl, itr;
-    itl = leftSorted.begin();
-    itr = rightSorted.begin();
-    while(1)
+    itl = left.begin();
+    itr = right.begin();
+
+    while(itl!=left.end() && itr!=right.end())
     {
-        if(itl==leftSorted.end())
-        {
-            while(itr!=rightSorted.end())
-            {
-                all.push_back(*itr);
-                itr++;
-            }
-            break;
-        }
-        if(itr==rightSorted.end())
-        {
-            while(itl!=leftSorted.end())
-            {
-                all.push_back(*itl);
-                itl++;
-            }
-            break;
-        }
         if(*itl<=*itr)
         {
             all.push_back(*itl);
@@ -65,6 +56,16 @@ vector<int> mergeSort(vector<int> A)
             all.push_back(*itr);
             itr++;
         }
+    }
+    while(itl!=left.end())
+    {
+        all.push_back(*itl);
+        itl++;
+    }
+    while(itr!=right.end())
+    {
+        all.push_back(*itr);
+        itr++;
     }
     return all;
 }
